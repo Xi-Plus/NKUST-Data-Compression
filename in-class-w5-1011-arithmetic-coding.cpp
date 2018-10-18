903bf4bf871273d2e23f9376c1f99797e6c1d5c4
xiplus <huangxuanyuxiplus@gmail.com>
2018-10-18 15:11:19 +0800

    Update in-class-w5-1011-arithmetic-coding.cpp
    +decode

--- in-class-w5-1011-arithmetic-coding.cpp
+++ in-class-w5-1011-arithmetic-coding.cpp
@@ -1,20 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-double encode(string s) {
+struct encodeobj{
+	map<char, pair<double, double> > table;
+	double value;
+};
+encodeobj encode(string s) {
 	int arr[s.length()];
 	for(int q=0; q<s.length(); q++) {
 		arr[q] = s[q];
 	}
 	sort(arr, arr+s.length());
+	encodeobj res;
+	for(int q=0; q<s.length(); q++) {
+		res.table[s[q]] = make_pair(1.0 * (lower_bound(arr, arr+s.length(), s[q])-arr) / s.length(), 1.0 * (upper_bound(arr, arr+s.length(), s[q])-arr) / s.length());
+	}
 	double low = 0, high = 1;
 	for(int q=0; q<s.length(); q++) {
 		double range = high - low;
 		high = low + range * (upper_bound(arr, arr+s.length(), s[q])-arr) / s.length();
 		low = low + range * (lower_bound(arr, arr+s.length(), s[q])-arr) / s.length();
 	}
-	return low;
+	res.value = low;
+	return res;
+}
+string decode(map<char, pair<double, double> > table, double value) {
+	string res = "";
+	while(value > 1e-7) {
+		for(map<char, pair<double, double> >::iterator it=table.begin(); it!=table.end(); ++it) {
+			if(value >= it->second.first && value < it->second.second) {
+				res += it->first;
+				value -= it->second.first;
+				value /= (it->second.second - it->second.first);
+				break;
+			}
+		}
+	}
+	return res;
 }
 int main() {
 	cout<<fixed<<setprecision(17);
-	cout<<encode("BILL GATES")<<endl;
+	encodeobj res = encode("BILL GATES");
+	cout<<res.value<<endl;
+	
+	cout<<decode(res.table, res.value)<<endl;
 }
