33741ac845a3c584a37096ddf7a2c613f2b85df8
xiplus <huangxuanyuxiplus@gmail.com>
2018-10-18 15:21:19 +0800

    Update in-class-w5-1011-arithmetic-coding.py
    +decode

--- in-class-w5-1011-arithmetic-coding.py
+++ in-class-w5-1011-arithmetic-coding.py
@@ -1,11 +1,29 @@
 def encode(s):
     arr = "".join(sorted(s))
+    table = {}
+    for c in s:
+    	table[c] = [(arr.index(c)) / len(s), (arr.rindex(c)+1)/len(s)]
     low = 0
     high = 1
     for c in s:
         range = high - low
         high = low + range * (arr.rindex(c)+1)/len(s)
         low = low + range * (arr.index(c)) / len(s)
-    return low
+    return {"table": table, "value": low}
 
-print(encode("BILL GATES"))
+def decode(table, value):
+	res = ""
+	while value > 1e-7:
+		for c in table:
+			if value >= table[c][0] and value < table[c][1]:
+				res += c
+				value -= table[c][0]
+				value /= (table[c][1] - table[c][0])
+				break
+	return res
+
+res = encode("BILL GATES")
+print(res["table"])
+print(res["value"])
+
+print(decode(res["table"], res["value"]))
