3609cbeaa0b003c03d474c3e6426a7dab1961ebe
xiplus <huangxuanyuxiplus@gmail.com>
2018-10-18 14:45:45 +0800

    Update in-class-w5-1011-arithmetic-coding.js
    +decode

--- in-class-w5-1011-arithmetic-coding.js
+++ in-class-w5-1011-arithmetic-coding.js
@@ -1,5 +1,9 @@
 function encode(s) {
     arr = s.split("").sort();
+    table = {};
+    for (c of s) {
+        table[c] = [arr.indexOf(c) / arr.length, (arr.lastIndexOf(c) + 1) / arr.length];
+    }
     low = 0;
     high = 1;
     for (c of s) {
@@ -7,6 +11,29 @@ function encode(s) {
         high = low + range * (arr.lastIndexOf(c) + 1) / arr.length;
         low = low + range * arr.indexOf(c) / arr.length;
     }
-    return low;
+    return {
+        "table": table,
+        "value": low
+    };
 }
-encode("BILL GATES");
+
+function decode(table, value) {
+    res = "";
+    while (value > 1e-7) {
+        for (c in table) {
+            if (value >= table[c][0] && value < table[c][1]) {
+                res += c;
+                value -= table[c][0];
+                value /= (table[c][1] - table[c][0]);
+                break;
+            }
+        }
+    }
+    return res;
+}
+
+res = encode("BILL GATES");
+console.log("encode table:", res["table"]);
+console.log("encode value:", res["value"]);
+
+console.log("decode:", decode(res["table"], res["value"]));
