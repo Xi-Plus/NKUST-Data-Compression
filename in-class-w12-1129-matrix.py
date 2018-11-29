f8401d300ad56e1f39dd170e68548cf1c56c0fd2
xiplus <huangxuanyuxiplus@gmail.com>
2018-11-29 17:33:42 +0800

    Create in-class-w12-1129-matrix.py

---
+++ in-class-w12-1129-matrix.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+a = [[1, 2, 3],
+     [4, 5, 6]]
+b = [[1, 2],
+     [3, 4],
+     [5, 6]]
+c = [1, 2, 3]
+d = [4, 5, 6]
+
+print(np.transpose(a))
+
+print(np.dot(c, d))
+
+print(np.dot(a, b))
